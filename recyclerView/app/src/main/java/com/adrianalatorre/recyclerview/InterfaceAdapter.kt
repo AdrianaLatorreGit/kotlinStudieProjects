58a86f0a5f0595b60acb6ab8e727ef72e77c5829
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2023-03-21 16:40:03 -0300

    Add files via upload

---
+++ recyclerView/app/src/main/java/com/adrianalatorre/recyclerview/InterfaceAdapter.kt
@@ -0,0 +1,16 @@
+package com.adrianalatorre.recyclerview
+
+class Musico(){
+
+    fun tocar(){
+        println("Tocando instrumento: ?")
+    }
+
+}
+
+fun main() {
+
+    val musico = Musico()
+    musico.tocar()
+
+}
